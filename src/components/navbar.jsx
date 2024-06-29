b014317ed8b30c2a83cebc40864700aa4ecdc868
ttroylim <troy.lim.2022@scis.smu.edu.sg>
2024-06-29 22:51:43 +0800

    created react project, tailwindcss + some navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import logo from "../assets/react.svg"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 item-center">
+            <img src={logo} alt="logo" />
+        </div>
+        <div className="m-8 flex item-center justify-center gap-4 text-2xl">
+            <FaLinkedin/>
+            <FaGithub/>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
