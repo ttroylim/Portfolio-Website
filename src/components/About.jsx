c125e58d66f6c432459b1991bfdb7137121bcae8
ttroylim <troy.lim.2022@scis.smu.edu.sg>
2024-07-12 21:40:31 +0800

    navbar, hero, about me completed

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+// stopped at 22:31 of video
+const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+        <h2 className="my-20 text-center text-4xl">
+            About <span className="text-neutral-500">Me</span>
+        </h2>
+        <div className="flex flex-warp">
+            <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="flex items-center justify-center">
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 item-center">
-            <img src={logo} alt="logo" />
+            {/* to edit logo */}
+            <img className="mx-2 w-10" src={logo} alt="logo" />
         </div>
         <div className="m-8 flex item-center justify-center gap-4 text-2xl">
             <FaLinkedin/>
